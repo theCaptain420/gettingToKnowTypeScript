6fbbd882230405ecbbe6e51eecf7b6cfbcaea3a4
thecaptain420 <mads.nielsen@hotmail.com>
2020-03-22 16:27:29 +0100

    tests setup, and  works

---
+++ src/multipleThreeFive/multipleThreeFive.spec.ts
@@ -0,0 +1,5 @@
+import totalSumDividedByNumbers from './multipleThreeFive'; 
+
+test("starter test", ()=>{
+expect(totalSumDividedByNumbers(1,1,1,1)).toBe(10);
+})
--- src/multipleThreeFive/multipleThreeFive.ts
+++ src/multipleThreeFive/multipleThreeFive.ts
@@ -2,9 +2,10 @@ let displayMessage: string;
 displayMessage ="hello"; 
 console.log( displayMessage)
 
-export default function totalSumDividedByNumbers(from, to, num1, num2):number{
+export default function totalSumDividedByNumbers(from:number, to:number, num1:number, num2:number):number{
     let finalNumber: number;
-
+    finalNumber= 0; 
+    
     for(let i = num1; i<num2; i++){
         if(i%num1==0){
            finalNumber+=i; 
