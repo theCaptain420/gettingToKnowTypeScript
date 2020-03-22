54d588e103a37ef17eeb141cdcff82a8f0ecf3de
thecaptain420 <mads.nielsen@hotmail.com>
2020-03-22 16:16:32 +0100

    module blueprint

---
+++ multipleThreeFive/multipleThreeFive.js
@@ -0,0 +1,3 @@
+var displayMessage;
+displayMessage = "hello";
+console.log(displayMessage);
---
+++ multipleThreeFive/multipleThreeFive.ts
@@ -0,0 +1,15 @@
+let displayMessage: string;
+displayMessage ="hello"; 
+console.log( displayMessage)
+
+export default function totalSumDividedByNumbers(from, to, num1, num2):number{
+    let finalNumber: number;
+
+    for(let i = num1; i<num2; i++){
+        if(i%num1==0){
+           finalNumber+=i; 
+        }
+    }
+    return 10; 
+}
+
