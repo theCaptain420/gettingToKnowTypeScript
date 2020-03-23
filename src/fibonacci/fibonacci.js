f8b352d5de627a4443da1c516cf80aac5c3994c0
thecaptain420 <mads.nielsen@hotmail.com>
2020-03-23 12:23:55 +0100

    exercise done and tests true on tests

--- src/fibonacci/fibonacci.js
+++ src/fibonacci/fibonacci.js
@@ -1,9 +1,14 @@
 "use strict";
 //https://projecteuler.net/problem=2
 exports.__esModule = true;
-function fibonnaciLastNumber() {
+function fibonnaciLastNumber(numberOf) {
     var finalNumber = 0;
+    var seqOne = 1;
+    var seqTwo = 2;
+    for (var i = 0; i < numberOf; i++) {
+        finalNumber += i;
+    }
     return finalNumber;
 }
 exports["default"] = fibonnaciLastNumber;
-console.log(fibonnaciLastNumber());
+console.log(fibonnaciLastNumber(3));
--- src/fibonacci/fibonacci.ts
+++ src/fibonacci/fibonacci.ts
@@ -1,9 +1,18 @@
 //https://projecteuler.net/problem=2
 
-export default function fibonnaciLastNumber():number{
+export default function fibonnaciLastNumber(numberOf :number):number{
     let finalNumber: number = 0;
+    let seqOne: number = 0;
+    let seqTwo: number = 1;
+
+    for(let i = 0; i<numberOf; i++){
+        finalNumber= seqOne+seqTwo;
+
+        seqOne= seqTwo;
+        seqTwo= finalNumber;
+    }
 
     return finalNumber 
 }
 
-console.log(fibonnaciLastNumber())
+console.log(fibonnaciLastNumber(3))
