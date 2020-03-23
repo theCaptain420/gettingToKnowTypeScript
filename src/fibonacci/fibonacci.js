787776ef0796f6ddd66e261b2f6d2c7c4168e0eb
thecaptain420 <mads.nielsen@hotmail.com>
2020-03-23 12:11:31 +0100

    writing so it works

---
+++ src/fibonacci/fibonacci.js
@@ -0,0 +1,9 @@
+"use strict";
+//https://projecteuler.net/problem=2
+exports.__esModule = true;
+function fibonnaciLastNumber() {
+    var finalNumber = 0;
+    return finalNumber;
+}
+exports["default"] = fibonnaciLastNumber;
+console.log(fibonnaciLastNumber());
--- src/fibonacci/fibonacci.ts
+++ src/fibonacci/fibonacci.ts
@@ -1,2 +1,9 @@
 //https://projecteuler.net/problem=2
 
+export default function fibonnaciLastNumber():number{
+    let finalNumber: number = 0;
+
+    return finalNumber 
+}
+
+console.log(fibonnaciLastNumber())
