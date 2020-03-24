13d4e0f85ae688556debae30467e297aeffd0b7d
thecaptain420 <mads.nielsen@hotmail.com>
2020-03-24 16:55:47 +0100

    test works, bad solution but still

--- src/smallestmultiple/smallestMultiple.ts
+++ src/smallestmultiple/smallestMultiple.ts
@@ -1,9 +1,13 @@
-console.log("hello wlrd")
 
-export default function smallestMultipleNumber(num1:number, num2:number):number{
-    let finalNumber: number = 0;
+export default function smallestMultipleNumber(numFrom: number, numTo: number): number {
+    let i :number = numFrom;
+ 
+    while (i %  2 != 0 || i %  3 != 0 || i %  4 != 0 || i %  5 != 0 ||
+             i %  6 != 0 || i %  7 != 0 || i %  8 != 0 || i %  9 != 0 ||
+             i % 10 != 0 ){
+        i++;
+    }
 
-    
-    
-    return finalNumber;
+
+    return i;
 }
