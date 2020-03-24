9f0874d9a18dd9d918750eb0b3313fff1948680c
thecaptain420 <mads.nielsen@hotmail.com>
2020-03-24 16:27:58 +0100

    setup test

---
+++ src/smallestmultiple/smallestMultiple.spec.ts
@@ -0,0 +1,5 @@
+import smallestMultiple from './smallestMultiple';
+
+test("is 2520 the smallest number that can be diveded by numbers 1 to 10", ()=>{
+    expect(smallestMultiple(1,10)).toBe(2520)
+})
--- src/smallestmultiple/smallestMultiple.ts
+++ src/smallestmultiple/smallestMultiple.ts
@@ -1,1 +1,9 @@
 console.log("hello wlrd")
+
+export default function smallestMultipleNumber(num1:number, num2:number):number{
+    let finalNumber: number = 0;
+
+    
+    
+    return finalNumber;
+}
